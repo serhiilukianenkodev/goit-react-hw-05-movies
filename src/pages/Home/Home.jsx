949821c40b81e529b8739f8c8252cbffc6c8116d
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2022-11-14 13:20:14 +0200

    start Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,3 +1,27 @@
+import { useState, useEffect } from 'react';
+import { getPopularFilms } from 'services/api';
+
 export const Home = () => {
-  return <p>Home page must be here</p>;
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  console.log(trendingMovies);
+
+  useEffect(() => {
+    getPopularFilms().then(data => setTrendingMovies(data.results));
+  }, []);
+
+  return (
+    <main>
+      <h1>Trending today</h1>
+      <ul>
+        {trendingMovies.map(({ title, poster_path, id }) => {
+          return (
+            <li key={id}>
+              {' '}
+              <a href={poster_path}>{title}</a>{' '}
+            </li>
+          );
+        })}
+      </ul>
+    </main>
+  );
 };
---
+++ src/services/api.js
@@ -0,0 +1,20 @@
+// const keyAPI = '3dd249fbd49ca96ba7400800c6400821'
+// example => https://api.themoviedb.org/3/movie/550?api_key=3dd249fbd49ca96ba7400800c6400821
+import axios from 'axios';
+
+const api = {
+  key: '3dd249fbd49ca96ba7400800c6400821',
+  baseUrl: 'https://api.themoviedb.org/3/',
+};
+
+export const getPopularFilms = async () => {
+  try {
+    const responce = await axios.get(
+      `${api.baseUrl}trending/movie/day?api_key=${api.key}`
+    );
+    console.log(responce.data);
+    return responce.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+};
